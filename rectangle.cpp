ea31bf64ecb8d0e55c85bccf9c6c9e8b9d454f41
zopawoli <tomasz.maciag@woli.edu.pl>
2025-11-17 08:33:07 +0000

    7777777b g

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,34 @@
 #include <iostream>
 
 class Rectangle {
+private:
+    double width;
+    double height;
 
+public:
+    rectangle(double w, double h)
+       : width(w), height(h) {}
+void setWidth(double w){
+width = w;
+}
+void setHeight(double h){
+    height = h;
+}
+double getwidth()const{
+    return width;
+}
+double getheight()const{
+    return height;
+}
+double area{}const{
+    return width*height
+}
+double perimeter{} const{
+    return 2* (width + height);
+}
+void display{} const{
+    cout << "Rectangle: " << width <<  " x " << height << endl;
+    cout << "area: " << area() << endl;
+    cout << "perimeter: " << perimeter() << endl;
+}
 };
